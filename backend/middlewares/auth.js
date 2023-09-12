75e7fa820593cd1dea488332e1f57b4fd7ff36af
Ankan chand <chandankan6294@gmail.com>
2023-09-12 07:08:23 +0530

    updating auth.js

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,17 +1,16 @@
 const User =require("../models/User"); // Import the User model
 const jwt = require("jsonwebtoken"); // Import the jsonwebtoken library
+const { catchAsyncError } = require("./catchAsyncError");
+const ErrorHandler = require("../utils/ErrorHandler");
 
 // Export a middleware function called `isAuthenticated` that checks if a user is authenticated or not
-exports.isAuthenticated = async (req,res,next) => {
-    try{
+exports.isAuthenticated = catchAsyncError(async (req,res,next) => {
         // Extract the `token` from the request cookies
         const {token} = req.cookies;
 
         // If there is no `token`, send a `401 Unauthorized` response with an error message
         if(!token) {
-            return res.status(401).json({
-                message:"Please login first!"
-            });
+            return next(new ErrorHandler("Please login first", 401));
         };
 
         // If there is a `token`, verify the token using the `jwt.verify()` method and the `JWT_SECRET` environment variable
@@ -22,10 +21,4 @@ exports.isAuthenticated = async (req,res,next) => {
 
         // Set the `user` object in the request object and call the `next()` function to pass control to the next middleware function
         next();
-    } catch(error) {
-        // If there is an error during any of these processes, send a `500 Internal Server Error` response with an error message
-        res.status(500).json({
-        message:error.message
-        });
-    }
-}
+});
